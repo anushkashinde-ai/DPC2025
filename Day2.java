8c1f23e9075a5837ff2e989b2a5813675b405c9b
Anushka Shinde <anushkashinde568@gmail.com>
2025-08-16 17:13:21 +0530

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,15 @@
+class Day2 {
+    public static int missingSum(int[] arr){
+        int n = arr.length+1;
+        long total = (long)n*(n+1)/2;
+        long have = 0;
+        for(int x : arr){
+            have += x;
+        }
+        return(int)(total-have);
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,4,5};
+        System.out.println("Missing number (Sum method): " + missingSum(arr));
+    }
+}
