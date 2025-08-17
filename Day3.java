125ceee2f6e26ea6fa0dc4dd2ee268c1e61301d3
Anushka Shinde <anushkashinde568@gmail.com>
2025-08-17 21:48:46 +0530

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,23 @@
+class Day3 {
+    public int findDuplicate(int[] nums) {
+      int slow = nums[0];
+      int fast = nums[0];
+
+      do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+      } while(slow!=fast);
+
+      slow = nums[0];
+      while(slow!=fast) {
+        slow =nums[slow];
+        fast =nums[fast];
+      }
+      return slow;
+    }
+    public static void main(String[] args){
+        int[] nums = {3,1,3,4,2};
+        Day3 d = new Day3(); 
+        System.out.println("Duplicate number: " + d.findDuplicate(nums));
+    }
+}
